f7d07ec3eb675ef4397348cee3162ca04977a269
Kyle Lee <sw.kylee@gmail.com>
2017-01-18 14:44:24 -0500

    add "Next" button

--- app/src/main/java/com/example/swkyl/androidlabs/ListItemsActivity.java
+++ app/src/main/java/com/example/swkyl/androidlabs/ListItemsActivity.java
@@ -1,13 +1,25 @@
 package com.example.swkyl.androidlabs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class ListItemsActivity extends AppCompatActivity {
+public class ListItemsActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
+        Button b3 = (Button) findViewById(R.id.button4);
+        b3.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        //v.getId();
+        Intent intent3 = new Intent(this, StartActivity.class);
+        startActivity(intent3);
     }
 }
--- app/src/main/java/com/example/swkyl/androidlabs/StartActivity.java
+++ app/src/main/java/com/example/swkyl/androidlabs/StartActivity.java
@@ -1,13 +1,26 @@
 package com.example.swkyl.androidlabs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class StartActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Button b1 = (Button) findViewById(R.id.button2);
+        b1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //v.getId();
+        Intent intent1 = new Intent(this, LoginActivity.class);
+        startActivity(intent1);
     }
 }
